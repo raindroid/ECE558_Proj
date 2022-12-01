0ed8c5b55477699f1b068ecaa669355ef581ed85
Raindroid <me@yucanwu.com>
2022-12-01 01:11:39 -0500

    Updated codebase

---
+++ sim_run.py
@@ -0,0 +1,127 @@
+# %% [markdown]
+# # ECE558 Project
+# By Tuo and Yucan
+
+# %% [markdown]
+# ## Libraries
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Other utilities
+import time
+from IPython.utils import io
+
+with io.capture_output() as captured:
+    import meep as mp
+    from meep import mpb
+
+from simulation import *
+
+# %%
+## For each setting
+# [ name: str,  blocks: int, w_range: [range of w], ws_range: [range of ws], geometry: lambda (w, ws, blocks): [mp.GeometricObject]]
+sc_y = 4
+sc_z = 4
+h = 0.150  # Si height (um) # 150 nm as in Bilodeau paper
+
+settings = [
+    ("S1", 1, np.linspace(0.1, 1.6, 76), [0], 
+     lambda w, ws, blocks: [mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(), material=Simulation.Si)], ),
+    ("S1+SiO2", 1, np.linspace(0.1, 1.6, 76), [0],
+     lambda w, ws, blocks: [mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(),  material=Simulation.Si), 
+                            mp.Block(size=mp.Vector3(mp.inf, mp.inf, 0.5*sc_z-0.5*h),center=mp.Vector3(0,0,0.25*sc_z+0.25*h), material=Simulation.SiO2)]),
+    
+    ("S2", 2, np.linspace(0.1, 1.4, 53), np.linspace(0.02, 0.98, 33),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w /2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)] ]),
+    ("S2+SiO2_b", 2, np.linspace(0.1, 1.4, 53), np.linspace(0.02, 0.98, 33),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w / 2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)],
+                            mp.Block(size=mp.Vector3(mp.inf, mp.inf, 0.5*sc_z-0.5*h),center=mp.Vector3(0,0,0.25*sc_z+0.25*h), material=Simulation.SiO2)]),
+    ("S2+SiO2_m", 2, np.linspace(0.1, 1.4, 53), np.linspace(0.02, 0.98, 33),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w / 2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)],
+                            *[mp.Block(size=mp.Vector3(mp.inf, ws, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w + ws / 2, 0), 
+                                       material=Simulation.SiO2) for x in range(blocks - 1)] ]),
+    ("S2+SiO2_bm", 2, np.linspace(0.1, 1.4, 53), np.linspace(0.02, 0.98, 33),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w / 2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)],
+                            *[mp.Block(size=mp.Vector3(mp.inf, ws, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w + ws / 2, 0), 
+                                       material=Simulation.SiO2) for x in range(blocks - 1)],
+                            mp.Block(size=mp.Vector3(mp.inf, mp.inf, 0.5*sc_z-0.5*h),center=mp.Vector3(0,0,0.25*sc_z+0.25*h), material=Simulation.SiO2)]),
+    
+    ("S3", 3, np.linspace(0.1, 1.0, 37), np.linspace(0.02, 0.47, 16),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w /2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)] ]),
+    ("S3+SiO2_b", 3, np.linspace(0.1, 1.0, 37), np.linspace(0.02, 0.47, 16),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w / 2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)],
+                            mp.Block(size=mp.Vector3(mp.inf, mp.inf, 0.5*sc_z-0.5*h),center=mp.Vector3(0,0,0.25*sc_z+0.25*h), material=Simulation.SiO2)]),
+    ("S3+SiO2_m", 3, np.linspace(0.1, 1.0, 37), np.linspace(0.02, 0.47, 16),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w / 2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)],
+                            *[mp.Block(size=mp.Vector3(mp.inf, ws, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w + ws / 2, 0), 
+                                       material=Simulation.SiO2) for x in range(blocks - 1)] ]),
+    ("S3+SiO2_bm", 3, np.linspace(0.1, 1.0, 37), np.linspace(0.02, 0.47, 16),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w / 2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)],
+                            *[mp.Block(size=mp.Vector3(mp.inf, ws, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w + ws / 2, 0), 
+                                       material=Simulation.SiO2) for x in range(blocks - 1)],
+                            mp.Block(size=mp.Vector3(mp.inf, mp.inf, 0.5*sc_z-0.5*h),center=mp.Vector3(0,0,0.25*sc_z+0.25*h), material=Simulation.SiO2)]),
+    
+    ("S4", 4, np.linspace(0.1, 0.7, 25), np.linspace(0.02, 0.38, 13),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w /2, 0), 
+                                material=Simulation.Si) for x in range(blocks)] ]),
+    ("S4+SiO2_b", 4, np.linspace(0.1, 0.7, 25), np.linspace(0.02, 0.38, 13),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w / 2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)],
+                            mp.Block(size=mp.Vector3(mp.inf, mp.inf, 0.5*sc_z-0.5*h),center=mp.Vector3(0,0,0.25*sc_z+0.25*h), material=Simulation.SiO2)]),
+    ("S4+SiO2_m", 4, np.linspace(0.1, 0.7, 25), np.linspace(0.02, 0.38, 13),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w / 2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)],
+                            *[mp.Block(size=mp.Vector3(mp.inf, ws, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w + ws / 2, 0), 
+                                       material=Simulation.SiO2) for x in range(blocks - 1)] ]),
+    ("S4+SiO2_bm", 4, np.linspace(0.1, 0.7, 25), np.linspace(0.02, 0.38, 13),
+     lambda w, ws, blocks: [ *[mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w / 2, 0), 
+                                        material=Simulation.Si) for x in range(blocks)],
+                            *[mp.Block(size=mp.Vector3(mp.inf, ws, h), center=mp.Vector3(0, 0 - (blocks * w + (blocks - 1) * ws) / 2 + x * (w + ws) + w + ws / 2, 0), 
+                                       material=Simulation.SiO2) for x in range(blocks - 1)],
+                            mp.Block(size=mp.Vector3(mp.inf, mp.inf, 0.5*sc_z-0.5*h),center=mp.Vector3(0,0,0.25*sc_z+0.25*h), material=Simulation.SiO2)]),
+]
+
+
+# %% [markdown]
+# ### Execute the settings
+
+# %%
+import asyncio
+def simulation_execute(setting, spinner=None):
+    sim = Simulation(sc_y=sc_y, sc_z=sc_z)
+    (setting_name, blocks, w_range, ws_range, geometry_lambda) = setting
+    neffs = np.zeros([len(w_range), len(ws_range), 2, 4])
+    for w_i, w in enumerate(w_range):
+        for ws_i, ws in enumerate(ws_range):
+            geometry = geometry_lambda(w, ws, blocks)
+            sim.init_mode_solver(geometry, default_material=Simulation.PC1)
+            neff = sim.run()
+            neffs[w_i][ws_i][0] = neff
+            
+            sim.init_mode_solver(geometry, default_material=Simulation.PC2)
+            neff = sim.run()
+            neffs[w_i][ws_i][1] = neff
+    
+    path = f"./data/sim_{setting_name}.npy"
+    np.save(path, neffs)
+    if spinner: 
+        spinner.succeed(f"Finished simulation with setting {setting_name}, saved to {path}")
+
+from halo import Halo
+with Halo(text='Simulating', spinner='dots') as spinner:
+    for i, setting in enumerate(settings):
+        spinner.info(f"Simulating {setting[0]} ...")
+        simulation_execute(setting, spinner)
+                        
+
+
---
+++ simulation.py
@@ -0,0 +1,121 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Other utilities
+import time
+from IPython.utils import io
+
+with io.capture_output() as captured:
+    import meep as mp
+    from meep import mpb
+
+
+class Block(object):
+    def __init__(self, w=mp.inf, h=mp.inf, n=1, center=mp.Vector3()):
+        """General block class to store parameters
+        
+        Parameters:
+        w (float) : width of the object (microns)
+        h (float) : heigth of the object (microns)
+        n (float) : refractive index of the object
+        """
+        self.w = w
+        self.h = h
+        self.n = n
+        self.center = center
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}(w={self.w}, h={self.h}, index={self.n})"
+    
+class Simulation(object):
+    
+    Si = mp.Medium(index=3.47343) # Silicon index @ 1550 nm
+    PC1 = mp.Medium(index=1.552) # Photochromic State 1 index
+    PC2 = mp.Medium(index=1.598) # Photochromic State 2 index
+    SiO2 = mp.Medium(index=1.444) # SiO2 bottom cladding index
+    
+    def __init__(self, N=4, wl=1.55, sc_y=4, sc_z=4, res=64):
+        """Script returning the first n effective indices of a silicon photonic strip waveguide
+    Accounts for material dispersion of the silicon using a Lorentz model
+
+        Args:
+            N (int, optional): number of modes to return. Defaults to 1.
+            w (float, optional): width of the waveguide. Defaults to 0.5.
+            h (float, optional): heigth of the waveguide. Defaults to 0.22.
+            wl (float, optional): wavelength of the simulation (microns). Defaults to 1.55.
+            sc_y (int, optional): width of the simulation (microns). Defaults to 4.
+            sc_z (int, optional): heigth of the simulation (microns). Defaults to 4.
+            res (int, optional): resolution of the simulation (pixels/micron). Defaults to 64.
+        """
+        self.supercell = Block(w=sc_y, h=sc_z)
+        self.N = N
+        self.wl = wl
+        self.resolution = res
+        self.ms = None
+        
+    def init_mode_solver(self, geometry=[], default_material=mp.Medium(index=1.552)):
+        # Simulation parameters
+        geometry_lattice = mp.Lattice(size=mp.Vector3(0, self.supercell.w, self.supercell.h))
+    
+        with io.capture_output() as _:
+            ms = mpb.ModeSolver(
+                geometry_lattice=geometry_lattice,
+                geometry=geometry,
+                resolution=self.resolution,
+                default_material=default_material) # PC is fully covering the waveguide.
+            self.ms = ms
+        return ms
+
+    def plot(self, figsize=(6,4), dpi=100, cmap='gray_r'):
+        assert self.ms, "Geometry not setup properly"
+        
+        with io.capture_output() as _:
+            self.ms.init_params(mp.NO_PARITY, True)
+
+        # Waveguide Geometry Visualization
+        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+
+        n = np.sqrt(self.ms.get_epsilon())
+
+        pos = ax.imshow(n.T, cmap=cmap, interpolation='spline36', 
+                        extent=[-self.supercell.w/2,self.supercell.w/2,
+                                -self.supercell.h/2,self.supercell.h/2] )
+        cbar = fig.colorbar(pos, ax=ax)
+        cbar.set_label('n')
+        ax.set_xlabel('y')
+        ax.set_ylabel('z')
+        plt.show()
+    
+    def run(self):
+        f_mode = 1/self.wl   # frequency corresponding to desired wavelength (in um) 
+        band_min = 1 
+        band_max = self.N
+        kdir = mp.Vector3(1) # Our waveguide is along the x direction
+        tol = 1e-6 # Iterative solver stop condition
+        kmag_guess = f_mode*3.45 # Initial guess
+        kmag_min = f_mode*0.1; kmag_max = f_mode*4.0 # Some search range
+
+        # Do the simulation
+        with io.capture_output() as _:
+            k_fmode = self.ms.find_k(mp.NO_PARITY, f_mode, band_min, band_max, 
+                                    kdir, tol, kmag_guess, kmag_min, kmag_max)
+
+        # Compute effective index
+        neff = []
+        for i in range(self.N):
+            neff.append(k_fmode[i] / f_mode)
+        
+        return neff
+    
+
+if __name__ == '__main__':
+    sim = Simulation()
+    
+    h = 0.150  # Si height (um) # 150 nm as in Bilodeau paper
+    w = 0.175 # waveguide width (um) # 175 nm as in Bilodeau paper 
+    geometry = [mp.Block(size=mp.Vector3(mp.inf, w, h), center=mp.Vector3(), 
+                         material=Simulation.Si)]
+    
+    sim.init_mode_solver(geometry, default_material=Simulation.PC1)
+    neff = sim.run()
+    print(neff)
