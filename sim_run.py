c48df73e8fc98d2509f62afbd14684a787a56ff1
Raindroid <me@yucanwu.com>
2022-12-06 14:19:50 -0500

    Updated simulation range

--- sim_run.py
+++ sim_run.py
@@ -58,14 +58,14 @@ ws4_range = np.linspace(0.06, 0.8, 38)
 settings = [
     ("S1", 1, np.linspace(0.06, 1.6, 78), [0], singleblock_geometry),
      
-    ("S2", 2, w2_range, ws2_range, multiblock_geometry),
-    ("S2_p", 2, w2_range, np.linspace(0.06, 0.40, 35), multiblock_m_geometry),
+    ("S2_1", 2, np.linspace(0.06, 0.3, 9), np.linspace(0.06, 0.99, 32), multiblock_geometry),
+    ("S2_m", 2, np.linspace(0.06, 0.99, 32), np.linspace(0.06, 0.99, 32), multiblock_m_geometry),
     
-    ("S3", 3, w3_range, ws3_range, multiblock_geometry),
-    ("S3_p", 3, w3_range, np.linspace(0.06, 0.35, 30), multiblock_m_geometry),
+    ("S3_1", 3, np.linspace(0.06, 0.3, 9), np.linspace(0.06, 0.48, 15), multiblock_geometry),
+    ("S3_m", 3, np.linspace(0.06, 0.84, 27), np.linspace(0.06, 0.48, 15), multiblock_m_geometry),
     
-    ("S4", 4, w4_range, ws4_range, multiblock_geometry),
-    ("S4_p", 4, w4_range, np.linspace(0.06, 0.35, 30), multiblock_m_geometry)
+    ("S4_1", 4, np.linspace(0.06, 0.3, 9), np.linspace(0.06, 0.39, 12), multiblock_geometry),
+    ("S4_m", 4, np.linspace(0.06, 0.75, 24), np.linspace(0.06, 0.39, 12), multiblock_m_geometry)
 ]
 
 # %% [markdown]
